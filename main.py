1b2c91b1fd9205ee5ef156d5e998a75e86e7ec6c
foshizzy <izzygit@fastmail.com>
2025-06-04 03:11:09 +0000

    added player and asteroid field with player collision

--- main.py
+++ main.py
@@ -4,8 +4,11 @@
 import pygame
 #-------------------------------
 #this allows use of modules
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 #--------------------------------
 def main():
     print("Starting Asteroids!")
@@ -14,7 +17,15 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     aclock = pygame.time.Clock()
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    field = AsteroidField()
     dt = 0
 
     #start of game loop
@@ -22,9 +33,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        updateable.update(dt)
+        for rock in asteroids:
+            if rock.collision(player) == True:
+                print("Game Over!")
+                sys.exit()
         screen.fill("black")
-        player.draw(screen)
+        for drawing in drawable:
+            drawing.draw(screen)
         pygame.display.flip()
         #limit framerate to 60 FPS
         dt = aclock.tick(60) / 1000
